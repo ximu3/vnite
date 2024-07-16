7c6273386fa896f3c3b1ce2fd8edf4cf0cc29150
ximu3 <ximu_simo@outlook.com>
2024-07-16 14:45:46 +0800

    add electron api

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,8 +1,12 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import { searchGameList, searchGameId } from "../renderer/src/components/scraper.mjs"
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  searchGameList,
+  searchGameId
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
