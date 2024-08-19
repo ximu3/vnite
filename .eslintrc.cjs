b6c066ead56f9c1ff428f0e7f673d3e151d99059
ximu3 <ximu_simo@outlook.com>
2024-08-19 18:45:45 +0800

    feat: 清理项目文件

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
