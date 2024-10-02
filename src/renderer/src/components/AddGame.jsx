c28c4ba9a90274350bfe1b1b7be611343ddf3e48
ximu3 <ximu_simo@outlook.com>
2024-10-02 10:28:34 +0800

    feat: 增加通过vid辅助刮削的功能

--- src/renderer/src/components/AddGame.jsx
+++ src/renderer/src/components/AddGame.jsx
@@ -151,6 +151,11 @@ function Info() {
           <input type="text" spellCheck='false' name='gid' className="grow" placeholder="月幕Galgame档案id，带GA" value={gid} onChange={(e) => { setGID(e.target.value) }} />
           <span className="border-0 badge bg-custom-blue-6 text-custom-text-light">可选</span>
         </label>
+        <label className="flex items-center w-full gap-2 border-0 input-sm input bg-custom-stress focus-within:outline-none hover:brightness-125 focus-within:border-0 focus-within:shadow-inner-sm focus-within:shadow-black focus-within:bg-custom-focus focus-within:text-custom-text-light/95 focus-within:hover:brightness-100">
+          <div className='font-semibold'>VID |</div>
+          <input type="text" spellCheck='false' name='gid' className="grow" placeholder="VNDB id，带v" value={vid} onChange={(e) => { setVID(e.target.value) }} />
+          <span className="border-0 badge bg-custom-blue-6 text-custom-text-light">可选</span>
+        </label>
         <div className='pt-1'>填写&nbsp;<span className='bg-custom-blue-6 text-custom-text-light'> GID </span>&nbsp;项可大幅提高识别正确率。</div>
       </div>
       <button id='discern' className='w-full h-10 transition-all mt-9 btn bg-custom-stress text-custom-text-light hover:brightness-125' onClick={submitInfo}>
@@ -222,7 +227,7 @@ function GameList() {
 }
 
 function GamePath() {
-  const { gamePath, savePath, gameName, setGamePath, setSavePath, setAlert, setIsLoading, setGameBgList, isLoading, gid } = useAddGame();
+  const { gamePath, savePath, gameName, setGamePath, setSavePath, setAlert, setIsLoading, setGameBgList, isLoading, gid, vid } = useAddGame();
   let navigate = useNavigate();
 
   async function selectGamePath() {
@@ -276,7 +281,12 @@ function GamePath() {
 
   async function submitGamePath() {
     setIsLoading(true);
-    const gameBgList = await window.api.getScreenshotsByTitle(gameName);
+    let gameBgList = [];
+    if (vid) {
+      gameBgList = await window.api.getScreenshotsByVID(vid);
+    } else {
+      gameBgList = await window.api.getScreenshotsByTitle(gameName);
+    }
     setGameBgList(gameBgList);
     navigate('/bg');
     setIsLoading(false);
@@ -304,7 +314,7 @@ function GamePath() {
 }
 
 function GameBg() {
-  const { gameBgList, gameName, savePath, isLoading, gamePath, setGameBg, gameBg, gid, setIsLoading } = useAddGame();
+  const { gameBgList, gameName, savePath, isLoading, gamePath, setGameBg, gameBg, gid, setIsLoading, vid } = useAddGame();
   let navigate = useNavigate();
   async function submitAllData() {
     if (gameBg === '' && gameBgList.length !== 0) {
@@ -313,7 +323,12 @@ function GameBg() {
       return
     }
     setIsLoading(true);
-    const coverUrl = await window.api.getCoverByTitle(gameName);
+    let coverUrl = '';
+    if (vid) {
+      coverUrl = await window.api.getCoverByVID(vid);
+    } else {
+      coverUrl = await window.api.getCoverByTitle(gameName);
+    }
     const id = await window.electron.ipcRenderer.invoke('generate-id', gameName);
     await window.electron.ipcRenderer.invoke('add-new-game-to-data', id, coverUrl, gameBg);
     await window.electron.ipcRenderer.send('organize-game-data', gid, savePath, gamePath);
