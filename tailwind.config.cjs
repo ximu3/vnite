827fc5a15771798135bdd89766cca5a232134e41
ximu3 <ximu_simo@outlook.com>
2024-10-24 16:06:43 +0800

    dev: remove useless fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
       }
     },
     fontFamily: {
-      sans: ['Source Han Sans VF', 'OPPOSans', 'sans-serif']
+      sans: ['Source Han Sans VF', 'sans-serif']
     },
     extend: {
       colors: {
