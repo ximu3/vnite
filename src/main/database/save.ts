2048d60b137fc56b5cf433c8e4a2fe032dfa00b0
ximu3 <ximu_simo@outlook.com>
2024-12-18 19:36:19 +0800

    feat: Keep only the last 7 archive backups

--- src/main/database/save.ts
+++ src/main/database/save.ts
@@ -5,14 +5,33 @@ import fse from 'fs-extra'
 
 export async function backupGameSave(gameId: string): Promise<void> {
   const saveId = generateUUID()
+
   const savePathMode = await getDBValue(`games/${gameId}/path.json`, ['savePath', 'mode'], 'folder')
   const savePath = await getDBValue(`games/${gameId}/path.json`, ['savePath', savePathMode], [''])
   const backupPath = await getDataPath(`games/${gameId}/saves/${saveId}/`)
+
+  // Get a list of the current archives
+  const saveList = await getDBValue(`games/${gameId}/save.json`, ['#all'], {})
+
+  // If the number of archives exceeds 7, delete the oldest archives
+  const saveIds = Object.keys(saveList)
+  if (saveIds.length >= 7) {
+    // 按日期排序，删除最旧的存档
+    saveIds.sort(
+      (a, b) => new Date(saveList[a].date).getTime() - new Date(saveList[b].date).getTime()
+    )
+    const oldestSaveId = saveIds[0]
+    delete saveList[oldestSaveId]
+    await fse.remove(await getDataPath(`games/${gameId}/saves/${oldestSaveId}/`))
+  }
+
+  // Backup new archive
   savePath.forEach(async (filePath) => {
     const fileName = path.basename(filePath)
     await fse.copy(filePath, path.join(backupPath, fileName))
   })
-  const saveList = await getDBValue(`games/${gameId}/save.json`, ['#all'], {})
+
+  // Add a new archive to the list
   saveList[saveId] = { id: saveId, date: new Date().toISOString(), note: '' }
   await setDBValue(`games/${gameId}/save.json`, ['#all'], saveList)
 }
