52ab4c41c53981e6f7e6b320de1cc4cede8a0ced
ximu3 <ximu_simo@outlook.com>
2025-01-23 12:18:30 +0800

    fix: Fix the problem that old databases were not purged when restoring databases

--- src/main/database/backup.ts
+++ src/main/database/backup.ts
@@ -1,5 +1,6 @@
 import { getDataPath, zipFolder, unzipFile } from '~/utils'
 import { app } from 'electron'
+import fse from 'fs-extra'
 
 export async function backupDatabase(targetPath: string, exclude?: string[]): Promise<void> {
   const dataPath = await getDataPath('')
@@ -10,6 +11,7 @@ export async function backupDatabase(targetPath: string, exclude?: string[]): Pr
 
 export async function restoreDatabase(sourcePath: string): Promise<void> {
   const dataPath = await getDataPath('')
+  await fse.remove(dataPath)
   await unzipFile(sourcePath, dataPath)
   app.relaunch()
   app.exit()
