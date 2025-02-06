1622e671b319c8ee97d39a9b80ee574b95f9849f
fishyy119 <1830979240@qq.com>
2025-02-06 13:15:05 +0800

    feat: Support drag-and-drop sorting of collections

--- src/renderer/src/components/Showcase/posters/CollectionPoster.tsx
+++ src/renderer/src/components/Showcase/posters/CollectionPoster.tsx
@@ -1,6 +1,18 @@
 import { HoverSquareCardAnimation } from '~/components/animations/HoverSquareCard'
 import { cn } from '~/utils'
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react'
+
+import invariant from 'tiny-invariant'
+import { draggable, dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
+import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine'
+import {
+  attachClosestEdge,
+  type Edge,
+  extractClosestEdge
+} from '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge'
+import { DropIndicator } from '@ui/drop-indicator'
+
 import { useCollections } from '~/hooks'
 import { CollectionCM } from '~/components/contextMenu/CollectionCM'
 import { GameImage } from '@ui/game-image'
@@ -13,68 +25,127 @@ export function CollectionPoster({
   className?: string
 }): JSX.Element {
   const navigate = useNavigate()
-  const { collections } = useCollections()
+  const { collections, reorderCollections } = useCollections()
   const collectionName = collections[collectionId].name
   const gameId = collections[collectionId].games[0]
   const length = collections[collectionId].games.length
+
+  const ref_ = useRef<HTMLDivElement>(null)
+  const [_dragging, setDragging] = useState<boolean>(false)
+  const [closestEdge, setClosestEdge] = useState<Edge | null>(null)
+
+  useEffect(() => {
+    const el = ref_.current
+    invariant(el)
+
+    return combine(
+      draggable({
+        element: el,
+        getInitialData: () => ({ collectionId }),
+        onDragStart: () => setDragging(true),
+        onDrop: () => setDragging(false)
+      }),
+      dropTargetForElements({
+        element: el,
+        getData: ({ input, element }) => {
+          // your base data you want to attach to the drop target
+          const data = {
+            id: collectionId
+          }
+          // this will 'attach' the closest edge to your `data` object
+          return attachClosestEdge(data, {
+            input,
+            element,
+            allowedEdges: ['right', 'left']
+          })
+        },
+        onDrag({ self, source }) {
+          const isSource = source.element === el
+          if (isSource) {
+            setClosestEdge(null)
+            return
+          }
+          const closestEdge = extractClosestEdge(self.data)
+          setClosestEdge(closestEdge)
+        },
+        onDragLeave() {
+          setClosestEdge(null)
+        },
+        onDrop({ self, source }) {
+          reorderCollections(
+            source.data.collectionId as string,
+            self.data.id as string,
+            extractClosestEdge(self.data) === 'left' ? 'front' : 'back'
+          )
+          setClosestEdge(null)
+        }
+      })
+    )
+  }, [])
+
   return (
     <CollectionCM collectionId={collectionId}>
-      <div
-        className={cn(
-          'group relative overflow-hidden shadow-custom-initial cursor-pointer w-[160px] h-[160px] rounded-lg',
-          'transition-all duration-300 ease-in-out',
-          'ring-0 ring-transparent',
-          'hover:ring-2 hover:ring-primary',
-          '3xl:w-[190px] 3xl:h-[190px]'
-        )}
-        onClick={() => navigate(`/library/collections/${collectionId}`)}
-      >
-        {/* background mask layer */}
-        <div
-          className={cn('absolute inset-0 bg-muted/40 backdrop-blur-sm z-10 pointer-events-none')}
-        />
-
-        {/* HoverBigCardAnimation layer */}
-
-        <div className="relative z-0">
-          <HoverSquareCardAnimation className={cn('rounded-none')}>
-            <GameImage
-              gameId={gameId}
-              type="cover"
-              alt={gameId}
-              className={cn(
-                'w-full h-full cursor-pointer object-cover',
-                '3xl:w-full 3xl:h-full',
-                className
-              )}
-              fallback={
-                <div
-                  className={cn(
-                    'w-full h-full cursor-pointer object-cover flex items-center justify-center',
-                    '3xl:w-full 3xl:h-full',
-                    className
-                  )}
-                ></div>
-              }
-            />
-          </HoverSquareCardAnimation>
-        </div>
-
-        {/* text content layer */}
+      <div className={cn('group relative')}>
         <div
           className={cn(
-            'absolute inset-0 z-20 mt-7',
-            'flex items-center justify-center',
-            'pointer-events-none'
+            'overflow-hidden shadow-custom-initial cursor-pointer w-[160px] h-[160px] rounded-lg',
+            'transition-all duration-300 ease-in-out',
+            'ring-0 ring-transparent',
+            'hover:ring-2 hover:ring-primary',
+            '3xl:w-[190px] 3xl:h-[190px]'
           )}
+          ref={ref_}
+          onClick={() => navigate(`/library/collections/${collectionId}`)}
         >
-          <div className="flex flex-col gap-1 items-center justify-center">
-            <div className={cn('text-accent-foreground text-lg font-semibold')}>
-              {collectionName}
+          {/* background mask layer */}
+          <div
+            className={cn('absolute inset-0 bg-muted/40 backdrop-blur-sm z-10 pointer-events-none')}
+          />
+
+          {/* HoverBigCardAnimation layer */}
+
+          <div className="relative z-0">
+            <HoverSquareCardAnimation className={cn('rounded-none')}>
+              <GameImage
+                gameId={gameId}
+                type="cover"
+                alt={gameId}
+                className={cn(
+                  'w-full h-full cursor-pointer object-cover',
+                  '3xl:w-full 3xl:h-full',
+                  className
+                )}
+                draggable="false"
+                fallback={
+                  <div
+                    className={cn(
+                      'w-full h-full cursor-pointer object-cover flex items-center justify-center',
+                      '3xl:w-full 3xl:h-full',
+                      className
+                    )}
+                  ></div>
+                }
+              />
+            </HoverSquareCardAnimation>
+          </div>
+
+          {/* text content layer */}
+          <div
+            className={cn(
+              'absolute inset-0 z-20 mt-7',
+              'flex items-center justify-center',
+              'pointer-events-none'
+            )}
+          >
+            <div className="flex flex-col gap-1 items-center justify-center">
+              <div className={cn('text-accent-foreground text-lg font-semibold')}>
+                {collectionName}
+              </div>
+              <div className={cn('text-accent-foreground/70')}>{`( ${length} )`}</div>
             </div>
-            <div className={cn('text-accent-foreground/70')}>{`( ${length} )`}</div>
           </div>
         </div>
+        {closestEdge && <DropIndicator edge={closestEdge} gap="24px" />}
       </div>
     </CollectionCM>
   )
---
+++ src/renderer/src/components/ui/drop-indicator.tsx
@@ -0,0 +1,55 @@
+// Modified from https://github.com/alexreardon/pdnd-react-tailwind/blob/main/src/drop-indicator.tsx
+import type { Edge } from '@atlaskit/pragmatic-drag-and-drop-hitbox/types'
+import type { CSSProperties, HTMLAttributes } from 'react'
+
+type Orientation = 'horizontal' | 'vertical'
+
+const edgeToOrientationMap: Record<Edge, Orientation> = {
+  top: 'horizontal',
+  bottom: 'horizontal',
+  left: 'vertical',
+  right: 'vertical'
+}
+
+const orientationStyles: Record<Orientation, HTMLAttributes<HTMLElement>['className']> = {
+  horizontal:
+    'h-[--line-thickness] left-[--terminal-radius] right-0 before:left-[--negative-terminal-size]',
+  vertical:
+    'w-[--line-thickness] top-[--terminal-radius] bottom-0 before:top-[--negative-terminal-size]'
+}
+
+const edgeStyles: Record<Edge, HTMLAttributes<HTMLElement>['className']> = {
+  top: 'top-[--line-offset] before:top-[--offset-terminal]',
+  right: 'right-[--line-offset] before:right-[--offset-terminal]',
+  bottom: 'bottom-[--line-offset] before:bottom-[--offset-terminal]',
+  left: 'left-[--line-offset] before:left-[--offset-terminal]'
+}
+
+const strokeSize = 4
+const terminalSize = 8
+const offsetToAlignTerminalWithLine = (strokeSize - terminalSize) / 2
+
+/**
+ * This is a tailwind port of `@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box`
+ */
+export function DropIndicator({ edge, gap }: { edge: Edge; gap: string }): JSX.Element {
+  const lineOffset = `calc(-0.5 * (${gap} + ${strokeSize}px))`
+
+  const orientation = edgeToOrientationMap[edge]
+
+  return (
+    <div
+      style={
+        {
+          '--line-thickness': `${strokeSize}px`,
+          '--line-offset': `${lineOffset}`,
+          '--terminal-size': `${terminalSize}px`,
+          '--terminal-radius': `${terminalSize / 2}px`,
+          '--negative-terminal-size': `-${terminalSize}px`,
+          '--offset-terminal': `${offsetToAlignTerminalWithLine}px`
+        } as CSSProperties
+      }
+      className={`absolute z-10 bg-blue-700 pointer-events-none before:content-[''] before:w-[--terminal-size] before:h-[--terminal-size] box-border before:absolute before:border-[length:--line-thickness] before:border-solid before:border-blue-700 before:rounded-full ${orientationStyles[orientation]} ${[edgeStyles[edge]]}`}
+    ></div>
+  )
+}
