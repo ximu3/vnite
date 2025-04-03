c17df70fe504728536464d72105a182e2d1c43be
ximu3 <ximu_simo@outlook.com>
2025-04-03 21:03:21 +0800

    feat: Card rounded corners support theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -82,6 +82,7 @@ module.exports = {
         }
       },
       borderRadius: {
+        xl: 'calc(var(--radius) + 6px)',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
