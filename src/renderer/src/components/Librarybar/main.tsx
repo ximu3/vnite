b9ba62044706e93ddb5d12a8c71ccbafc1ce806c
ximu3 <ximu_simo@outlook.com>
2025-07-19 14:41:57 +0800

    fix: Refresh game list on add or delete events
    Adds event listeners for 'game:added' and 'game:deleted' to automatically refresh the game list in the Librarybar component when games are added or removed.

--- src/renderer/src/components/Librarybar/main.tsx
+++ src/renderer/src/components/Librarybar/main.tsx
@@ -3,12 +3,29 @@ import { cn } from '~/utils'
 import { GameList } from './GameList'
 import { PositionButton } from './PositionButton'
 import { useLibrarybarStore } from './store'
+import { eventBus } from '~/app/events'
+import { useEffect } from 'react'
 
 export function Librarybar(): React.JSX.Element {
   const [selectedGroup] = useConfigState('game.gameList.selectedGroup')
   const query = useLibrarybarStore((state) => state.query)
+  const refreshGameList = useLibrarybarStore((state) => state.refreshGameList)
 
   console.warn(`[DEBUG] Librarybar`)
+
+  useEffect(() => {
+    const unsubscribeGameAdded = eventBus.on('game:added', () => {
+      refreshGameList()
+    })
+    const unsubscribeGameDeleted = eventBus.on('game:deleted', () => {
+      refreshGameList()
+    })
+
+    return () => {
+      unsubscribeGameAdded()
+      unsubscribeGameDeleted()
+    }
+  }, [])
 
   return (
     <div className={cn('flex flex-col gap-6 bg-transparent w-[270px] h-full relative group')}>
