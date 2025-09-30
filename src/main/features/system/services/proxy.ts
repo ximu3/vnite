3ae6327883c7f8f7c6af755b29f62f485b123f63
mihirukiss <kiss@mihiru.com>
2025-09-30 15:24:26 +0800

    feat: support proxy(add try catch to setupProxy)

--- src/main/features/system/services/proxy.ts
+++ src/main/features/system/services/proxy.ts
@@ -2,23 +2,27 @@ import { session } from 'electron'
 import { ConfigDBManager } from '~/core/database'
 
 export async function setupProxy(): Promise<void> {
-  const proxyEnable = await ConfigDBManager.getConfigLocalValue('network.proxy.enable')
-  const protocol = await ConfigDBManager.getConfigLocalValue('network.proxy.protocol')
-  const host = await ConfigDBManager.getConfigLocalValue('network.proxy.host')
-  const port = await ConfigDBManager.getConfigLocalValue('network.proxy.port')
-  if (!proxyEnable || !protocol || !host || !port || isNaN(port) || port < 1 || port > 65535) {
-    await session.defaultSession.setProxy({ mode: 'system' })
+  try {
+    const proxyEnable = await ConfigDBManager.getConfigLocalValue('network.proxy.enable')
+    const protocol = await ConfigDBManager.getConfigLocalValue('network.proxy.protocol')
+    const host = await ConfigDBManager.getConfigLocalValue('network.proxy.host')
+    const port = await ConfigDBManager.getConfigLocalValue('network.proxy.port')
+    if (!proxyEnable || !protocol || !host || !port || isNaN(port) || port < 1 || port > 65535) {
+      await session.defaultSession.setProxy({ mode: 'system' })
+      await session.defaultSession.closeAllConnections()
+      return
+    }
+    const bypassRules = await ConfigDBManager.getConfigLocalValue('network.proxy.bypassRules')
+    const proxyRules = `${protocol}://${host}:${port}`
+    const proxyConfig: Electron.ProxyConfig = {
+      proxyRules
+    }
+    if (bypassRules && bypassRules.trim().length > 0) {
+      proxyConfig.proxyBypassRules = bypassRules
+    }
+    await session.defaultSession.setProxy(proxyConfig)
     await session.defaultSession.closeAllConnections()
-    return
+  } catch (error) {
+    console.error('Error setting up proxy:', error)
   }
-  const bypassRules = await ConfigDBManager.getConfigLocalValue('network.proxy.bypassRules')
-  const proxyRules = `${protocol}://${host}:${port}`
-  const proxyConfig: Electron.ProxyConfig = {
-    proxyRules
-  }
-  if (bypassRules && bypassRules.trim().length > 0) {
-    proxyConfig.proxyBypassRules = bypassRules
-  }
-  await session.defaultSession.setProxy(proxyConfig)
-  await session.defaultSession.closeAllConnections()
 }
