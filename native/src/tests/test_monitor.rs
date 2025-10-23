085ceba66ab1ee24621de3b8f665ba8c6e5146e0
Vibbit <50281195+vilebbit@users.noreply.github.com>
2025-10-23 11:39:18 +0900

    test(native): foreground hook unit test

--- native/src/tests/test_monitor.rs
+++ native/src/tests/test_monitor.rs
@@ -1,3 +1,4 @@
+use crate::foreground;
 use crate::monitor;
 
 #[tokio::test]
@@ -9,5 +10,20 @@ async fn test_monitor() {
     None
   ).await;
   tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+  monitor::stop_monitoring().await;
+}
+
+#[tokio::test]
+async fn test_foreground_hook() {
+  monitor::start_monitoring(
+    vec![r"c:\program files\windowsapps\microsoft.windowscalculator_11.2502.2.0_x64__8wekyb3d8bbwe\calculatorapp.exe".to_string(),
+    r"C:\Program Files\WindowsApps\Microsoft.WindowsNotepad_11.2507.26.0_x64__8wekyb3d8bbwe\Notepad\Notepad.exe".to_string()], 
+    vec!["123456-abcdef".to_string()],
+    None
+  ).await;
+  foreground::install_hook(None, None).await;
+
+  tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+  foreground::uninstall_hook().await;
   monitor::stop_monitoring().await;
 }
