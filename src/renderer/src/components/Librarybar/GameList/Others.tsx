a611048ab8a17c22269aa2baf07e246feed05a1c
yyy-win11 <1830979240@qq.com>
2025-11-08 16:52:28 +0800

    feat(gamelist): add dedicated accordion to quickly view games without the field

--- src/renderer/src/components/Librarybar/GameList/Others.tsx
+++ src/renderer/src/components/Librarybar/GameList/Others.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import {
   Accordion,
   AccordionContent,
@@ -16,14 +17,19 @@ import { RecentGames } from './RecentGames'
 export function Others({
   fieldName
 }: {
-  fieldName: 'metadata.developers' | 'metadata.genres'
+  fieldName: 'metadata.developers' | 'metadata.genres' // It can also be 'none'
 }): React.JSX.Element {
   const [by] = useConfigState('game.gameList.sort.by')
   const [order] = useConfigState('game.gameList.sort.order')
   const [showAllGamesInGroup] = useConfigState('game.gameList.showAllGamesInGroup')
   const [nsfwFilterMode] = useConfigState('appearances.nsfwFilterMode')
+  const { t } = useTranslation('game')
+  const emptyAccordionName = {
+    'metadata.developers': t('list.empty.developers'),
+    'metadata.genres': t('list.empty.genres')
+  }
 
-  const fields = getAllValuesInKey(fieldName)
+  const fields = [...getAllValuesInKey(fieldName), '__empty__']
   const defaultValues = [...fields, 'all', 'recentGames']
 
   const setOpenValues = useGameListStore((s) => s.setOpenValues)
@@ -46,26 +52,33 @@ export function Others({
           {/* Recent Games */}
           <RecentGames />
           {/* Split games into their respective fields */}
-          {fields.map((field) => {
-            const gameIds = filterGamesByNSFW(nsfwFilterMode, filterGames({ [fieldName]: [field] }))
-            if (gameIds.length === 0) return <></>
+          {(fieldName === 'metadata.developers' || fieldName === 'metadata.genres') &&
+            fields.map((field) => {
+              const gameIds = filterGamesByNSFW(
+                nsfwFilterMode,
+                filterGames({ [fieldName]: [field] })
+              )
+              if (gameIds.length === 0) return <></>
 
-            return (
-              <AccordionItem key={field} value={field}>
-                <AccordionTrigger defaultChecked className={cn('text-xs p-1 pl-2')}>
-                  <div className={cn('flex flex-row items-center justify-start gap-1')}>
-                    <div className={cn('text-xs')}>{field}</div>
-                    <div className={cn('text-2xs text-foreground/50')}>({gameIds.length})</div>
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent className={cn('rounded-none pt-1 flex flex-col gap-1')}>
-                  {sortGames(by, order, gameIds).map((game) => (
-                    <GameNav key={game} gameId={game} groupId={`${fieldName}:${field}`} />
-                  ))}
-                </AccordionContent>
-              </AccordionItem>
-            )
-          })}
+              return (
+                <AccordionItem key={field} value={field}>
+                  <AccordionTrigger defaultChecked className={cn('text-xs p-1 pl-2')}>
+                    <div className={cn('flex flex-row items-center justify-start gap-1')}>
+                      <div className={cn('text-xs')}>
+                        {field !== '__empty__' ? field : emptyAccordionName[fieldName]}
+                      </div>
+                      <div className={cn('text-2xs text-foreground/50')}>({gameIds.length})</div>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent className={cn('rounded-none pt-1 flex flex-col gap-1')}>
+                    {sortGames(by, order, gameIds).map((game) => (
+                      <GameNav key={game} gameId={game} groupId={`${fieldName}:${field}`} />
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
+              )
+            })}
+
           {/* All Games */}
           {showAllGamesInGroup && <AllGame />}
         </Accordion>
