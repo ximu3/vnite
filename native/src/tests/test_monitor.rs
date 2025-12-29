f360b3ac88b827b84832991e1922ee22a9a3acfe
Vibbit <50281195+vilebbit@users.noreply.github.com>
2025-12-29 16:23:57 +0900

    test: test is_running native function

--- native/src/tests/test_monitor.rs
+++ native/src/tests/test_monitor.rs
@@ -4,20 +4,20 @@ use crate::monitor;
 #[tokio::test]
 async fn test_monitor() {
   monitor::start_monitoring(
-    vec![r"c:\program files\windowsapps\microsoft.windowscalculator_11.2502.2.0_x64__8wekyb3d8bbwe\calculatorapp.exe".to_string(),
-    r"C:\Program Files\WindowsApps\Microsoft.WindowsNotepad_11.2507.26.0_x64__8wekyb3d8bbwe\Notepad\Notepad.exe".to_string()], 
+    vec![r"c:\program files\cmake\bin\cmake-gui.exe".to_string()], 
     vec!["123456-abcdef".to_string()],
     None
   ).await;
   tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+  let is_running = monitor::gm::get().lock().await.is_running("C:\\Program Files\\CMake\\bin", Some(true));
+  println!("is running: {}", is_running);
   monitor::stop_monitoring().await;
 }
 
 #[tokio::test]
 async fn test_foreground_hook() {
   monitor::start_monitoring(
-    vec![r"c:\program files\windowsapps\microsoft.windowscalculator_11.2502.2.0_x64__8wekyb3d8bbwe\calculatorapp.exe".to_string(),
-    r"C:\Program Files\WindowsApps\Microsoft.WindowsNotepad_11.2507.26.0_x64__8wekyb3d8bbwe\Notepad\Notepad.exe".to_string()], 
+    vec![r"c:\program files\cmake\bin\cmake-gui.exe".to_string()], 
     vec!["123456-abcdef".to_string()],
     None
   ).await;
